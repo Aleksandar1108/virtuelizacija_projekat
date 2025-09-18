d9b2effec910f0e38db2c0506c5f9f97408ca496
Milos Jovanovic <Milos Jovanovic@DESKTOP-R82LKLU>
2025-09-18 20:58:01 +0200

    prva verzija

---
+++ Common/IBatteryStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public interface IBatteryStorage : IDisposable
+    {
+        void InitializeSession(EisMeta meta);
+        void StoreSample(EisSample sample);
+        void StoreRejectedSample(string reason, string rawData);
+        void StoreAnalyticsEvent(string alertType, string message, double value, double threshold);
+        void FinalizeSession();
+        string GetSessionDirectory();
+        int GetSampleCount();
+    }
+
+}
